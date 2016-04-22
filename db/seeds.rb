30b71f3f07875b65b4876b1cd2bb2cb20357bdeb
Matthew Swan <swanm86@gmail.com>
2016-04-21 22:35:13 -0500

    db migration, route alterations, and seeded database with Cubs roster

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Player.create(name: "Dexter Fowler", number: 24, average: 392)
+Player.create(name: "Ben Zobrist", number: 18, average: 246)
+Player.create(name: "Jason Heyward", number: 22, average: 200)
+Player.create(name: "Anthony Rizzo", number: 44, average: 186)
+Player.create(name: "Kris Bryant", number: 17, average: 273)
+Player.create(name: "Addison Russell", number: 27, average: 196)
+Player.create(name: "Jorge Soler", number: 68, average: 196)
+Player.create(name: "Miguel Montero", number: 47, average: 256)
+Player.create(name: "Jake Arrieta", number: 49, average: 273)
+
